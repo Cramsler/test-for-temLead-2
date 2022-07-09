a721e9ca4820c257ca5d293b6ff66dd080cedffc
Glazunov Aleksandr <glazunov@elma-bpm.com>
2022-07-09 17:17:48 +0400

    init project

---
+++ main.js
@@ -0,0 +1,197 @@
+class Api {
+    getApiResponse() {
+        return {
+            "groupingDateByOrderCreatedAt": "",
+            "groupingCallCenterId": "",
+            "billApprovedUsdAvg": 64.93,
+            "groupings": {
+                "2022-06-08 00:00:00": {
+                    "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                    "groupingCallCenterId": "",
+                    "billApprovedUsdAvg": 64.93,
+                    "groupings": {
+                        "12fd2e12-6867-4fae-b76e-1ae56ed92628": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "12fd2e12-6867-4fae-b76e-1ae56ed92628",
+                            "billApprovedUsdAvg": 66.58,
+                            "groupings": []
+                        },
+                        "176e81cd-a459-4f0e-8dd1-b3b6207f68ea": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "176e81cd-a459-4f0e-8dd1-b3b6207f68ea",
+                            "billApprovedUsdAvg": 43.56,
+                            "groupings": []
+                        },
+                        "1ee8f0b4-5a1c-4f55-a05b-e2b920ed27cf": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "1ee8f0b4-5a1c-4f55-a05b-e2b920ed27cf",
+                            "billApprovedUsdAvg": 63.86,
+                            "groupings": []
+                        },
+                        "25d519ce-eee2-4f9f-9127-7a075a9ca2c2": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "25d519ce-eee2-4f9f-9127-7a075a9ca2c2",
+                            "billApprovedUsdAvg": 75.78,
+                            "groupings": []
+                        },
+                        "5712b138-58b8-4f51-9b1c-63ae521c24ad": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "5712b138-58b8-4f51-9b1c-63ae521c24ad",
+                            "billApprovedUsdAvg": 136.1,
+                            "groupings": []
+                        },
+                        "58edf840-0921-4fba-a833-9cdad7d6d0b1": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "58edf840-0921-4fba-a833-9cdad7d6d0b1",
+                            "billApprovedUsdAvg": 65.35,
+                            "groupings": []
+                        },
+                        "643b8e90-a94f-4fd4-b8d2-fbbb78d7dcca": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "643b8e90-a94f-4fd4-b8d2-fbbb78d7dcca",
+                            "billApprovedUsdAvg": 57.65,
+                            "groupings": []
+                        },
+                        "70ca403e-5ad3-4f9f-b2c1-fc9528c65d0e": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "70ca403e-5ad3-4f9f-b2c1-fc9528c65d0e",
+                            "billApprovedUsdAvg": 45.04,
+                            "groupings": []
+                        },
+                        "7167f73b-0f2b-4f7a-a580-77828b89b838": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "7167f73b-0f2b-4f7a-a580-77828b89b838",
+                            "billApprovedUsdAvg": 129.32,
+                            "groupings": []
+                        },
+                        "760188a4-cb2e-4f7a-917d-743225c7bd7b": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "760188a4-cb2e-4f7a-917d-743225c7bd7b",
+                            "billApprovedUsdAvg": 51.06,
+                            "groupings": []
+                        },
+                        "7c2ed346-346d-4f8d-8d9b-6e1511f340ac": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "7c2ed346-346d-4f8d-8d9b-6e1511f340ac",
+                            "billApprovedUsdAvg": 50.19,
+                            "groupings": []
+                        },
+                        "96e60c50-e963-4fe7-91d1-a94135729097": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "96e60c50-e963-4fe7-91d1-a94135729097",
+                            "billApprovedUsdAvg": 47.19,
+                            "groupings": []
+                        },
+                        "984bc617-7215-4fae-9b9e-dcb45dd875e9": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "984bc617-7215-4fae-9b9e-dcb45dd875e9",
+                            "billApprovedUsdAvg": 47.53,
+                            "groupings": []
+                        },
+                        "9a4fa441-d0ec-4f9e-9ae2-eb41ed45c804": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "9a4fa441-d0ec-4f9e-9ae2-eb41ed45c804",
+                            "billApprovedUsdAvg": 106.92,
+                            "groupings": []
+                        },
+                        "ad6b93f8-2c22-4fa8-a8bc-ce567a28f08d": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "ad6b93f8-2c22-4fa8-a8bc-ce567a28f08d",
+                            "billApprovedUsdAvg": 62.51,
+                            "groupings": []
+                        },
+                        "b181e94d-cf93-4f85-8bff-bd9634d443cf": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "b181e94d-cf93-4f85-8bff-bd9634d443cf",
+                            "billApprovedUsdAvg": 90.97,
+                            "groupings": []
+                        },
+                        "c9461d4e-5fbc-4f74-98fd-c514e282099c": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "c9461d4e-5fbc-4f74-98fd-c514e282099c",
+                            "billApprovedUsdAvg": 72.17,
+                            "groupings": []
+                        },
+                        "e55523b1-3c82-4ffb-b7b4-43be6eb0cdac": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "e55523b1-3c82-4ffb-b7b4-43be6eb0cdac",
+                            "billApprovedUsdAvg": 68.87,
+                            "groupings": []
+                        },
+                        "ed69bf72-e598-4f10-8f76-05de5a70bdb5": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "ed69bf72-e598-4f10-8f76-05de5a70bdb5",
+                            "billApprovedUsdAvg": 69.16,
+                            "groupings": []
+                        },
+                        "f00c215c-5cac-4fee-a79c-9684e62dc1f0": {
+                            "groupingDateByOrderCreatedAt": "2022-06-08 00:00:00",
+                            "groupingCallCenterId": "f00c215c-5cac-4fee-a79c-9684e62dc1f0",
+                            "billApprovedUsdAvg": 52.31,
+                            "groupings": []
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+class Parser {
+
+    constructor(tree) {
+        this.parsedThree = this.parseData(tree)
+    }
+
+    parseData(tree) {
+        let result = [{
+            "groupingDateByOrderCreatedAt": tree.groupingDateByOrderCreatedAt,
+            "groupingCallCenterId": tree.groupingCallCenterId,
+            "billApprovedUsdAvg": tree.billApprovedUsdAvg,
+        }];
+
+        if (Object.keys(tree.groupings).length) {
+            for (let item in tree.groupings) {
+                result = result.concat(this.parseData(tree.groupings[item]))
+            }
+        }
+
+        return result;
+    }
+}
+
+class HtmlBuilder {
+    insertDataToTable(data) {
+        const table = document.getElementById('data-table');
+        let tbody = this.createEl('tbody');
+
+        data.forEach((object) => {
+            let tr = this.createEl('tr');
+
+            for (let item in object) {
+                let td = this.createEl('td');
+                let content = this.createText(object[item]);
+
+                td.appendChild(content);
+                tr.appendChild(td);
+            }
+
+            tbody.appendChild(tr)
+            table.appendChild(tbody)
+        })
+    }
+
+    createEl(el) {
+        return document.createElement(el);
+    }
+
+    createText(text) {
+       return document.createTextNode(text)
+    }
+}
+
+const api = new Api();
+const parser = new Parser(api.getApiResponse());
+const builder = new HtmlBuilder();
+
+builder.insertDataToTable(parser.parsedThree);
